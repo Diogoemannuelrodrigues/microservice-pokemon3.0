44c5df359a8ec8e171efdd656ccee3c2b77075c9
diogoemannuel <diogoemannuel.rodrigues1@gmail.com>
2023-05-13 00:56:35 -0300

    first commit

---
+++ attack/src/main/java/br/com/microservice/attack/application/AttackApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.attack.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttackApplication.class, args);
+	}
+
+}
---
+++ attack/src/test/java/br/com/microservice/attack/AttackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservice.attack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
