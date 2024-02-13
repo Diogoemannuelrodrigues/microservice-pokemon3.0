36ec0db719a0e70908cadbeed69ebbb885c2a693
Diogo Emannuel <diogoemannuel.rodrigues1@gmail.com>
2024-02-13 06:21:12 -0300

    Ajuste na ordem dos routes no gateway

--- mscloudgateway/src/main/java/io/github/com/diogorodrigues/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/com/diogorodrigues/MscloudgatewayApplication.java
@@ -10,29 +10,29 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableEurekaClient
 public class MscloudgatewayApplication {
-	public static final String MSPOKEMON = "lb://mspokemon";
+    public static final String MSPOKEMON = "lb://mspokemon";
 
-	public static void main(String[] args) {
-		SpringApplication.run(MscloudgatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MscloudgatewayApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator routes(RouteLocatorBuilder builder){
-		return builder
-				.routes()
-					.route(r -> r.path("/api/v1/pokemon").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/pokemon/name/{name}").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/pokemon/all").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/pokemon/{id}").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/pokemon/evolutions/{name}").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/pokemon/information/{name}").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/treinador").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/treinador/name/{name}").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/treinador/{id}").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/treinador/adiciona/{treinador}").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/pokemon/verificaEvolution/{treinador}/pokemon/{pokemonName}/stone/{stone}").uri(MSPOKEMON))
-					.route(r -> r.path("/api/v1/pokemon/evoluir/{pokemonName}/teinador/{treinador}").uri(MSPOKEMON))
-				.build();
-	}
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/api/v1/pokemon").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/pokemon/name/{name}").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/pokemon/all").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/pokemon/{id}").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/pokemon/evolutions/{name}").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/pokemon/information/{name}").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/pokemon/verificaEvolution/{treinador}/pokemon/{pokemonName}/stone/{stone}").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/pokemon/evoluir/{pokemonName}/teinador/{treinador}").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/treinador").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/treinador/name/{name}").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/treinador/{id}").uri(MSPOKEMON))
+                .route(r -> r.path("/api/v1/treinador/adiciona/{treinador}").uri(MSPOKEMON))
+                .build();
+    }
 
 }
