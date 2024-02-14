3bfdf45ceaa2186424a3f8fb78e2dda451e889d9
Diogo Emannuel <diogoemannuel.rodrigues1@gmail.com>
2024-02-14 17:08:49 -0300

    :bug: Ajustando o ms de atack

--- attack/src/main/java/br/com/microservice/attack/application/AttackApplication.java
+++ attack/src/main/java/br/com/microservice/attack/application/AttackApplication.java
@@ -3,9 +3,11 @@ package br.com.microservice.attack.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan(basePackages = {"br.com.microservice.attack.application.repository.AttackRepository"})
 public class AttackApplication {
 
 	public static void main(String[] args) {
